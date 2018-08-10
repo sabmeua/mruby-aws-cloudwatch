c510a080f037469220cd598c4ba928224943e310
sabmeua <sabme.ua@gmail.com>
2018-08-10 03:52:43 +0000

    Add MetricDataQuery module

---
+++ mrblib/aws-cloudwatch-metricdataquery.rb
@@ -0,0 +1,34 @@
+module AWS::CloudWatch
+
+  module MetricDataQuery
+
+    module Expose
+      def build(query)
+        result = {}
+        serialize query, "MetricDataQueries", result
+      end
+
+      def serialize(elm, str, buff)
+        i = 0
+        elm.each do |k,v|
+          if v.nil?
+            v = k
+            i = i + 1
+            k = "member.#{i}"
+          end
+          if v.kind_of?(Enumerable)
+            buff.merge(serialize v, "#{str}.#{k}", buff)
+          else
+            buff["#{str}.#{k}"] = v
+          end
+        end
+        buff
+      end
+    end
+
+    extend Expose
+
+  end
+
+end
+
