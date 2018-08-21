78edd3c2bc0da3526a7985d3808cb8e0bc695b72
sabmeua <sabme.ua@gmail.com>
2018-08-21 05:10:45 +0000

    Add get_metric_statistics method

--- mrblib/aws-cloudwatch-functions.rb
+++ mrblib/aws-cloudwatch-functions.rb
@@ -26,6 +26,32 @@ module AWS::CloudWatch
       request('POST', params)
     end
 
+    def get_metric_statistics(start_time, end_time,
+        name_space, metric_name, period, statistics = nil, dimensions = nil)
+      start_time = Time.at(start_time) if !start_time.instance_of? Time
+      end_time = Time.at(end_time) if !end_time.instance_of? Time
+      params = {
+        :Action => 'GetMetricStatistics',
+        :Version => AWS::CloudWatch::API_VERSION_LATEST,
+        :StartTime => start_time.utc.strftime(ISO8601_UTC),
+        :EndTime => end_time.utc.strftime(ISO8601_UTC),
+        :Namespace => name_space,
+        :MetricName => metric_name,
+        :Period => period
+      }
+      if statistics
+        statistics = statistics.split ' ' if statistics.kind_of? String
+        params.merge!(AWS::CloudWatch::MetricDataQuery::serialize(statistics, 'Statistics', {}))
+      end
+      if dimensions
+        if dimensions.kind_of? String
+          dimensions = dimensions.split(',').map{|v| v.split '='}.to_h
+        end
+        params.merge!(AWS::CloudWatch::MetricDataQuery::serialize([dimensions], 'Dimensions', {}))
+      end
+      request('POST', params)
+    end
+
   end
 
 end
